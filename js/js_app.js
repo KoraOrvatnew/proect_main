8d588cb497faeba716945460e1ded3f7ce9b97c4
KoraOrvatnew <mironovich.alena83@gmail.com>
2025-02-26 15:17:46 +0300

    header_footer

--- js/js_app.js
+++ js/js_app.js
@@ -1,6 +1,6 @@
 const API_KEY = "529c790f-3578-406b-94dd-d8956b7d306b";
 const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1";
-const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword="
+const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 getMovies(API_URL_POPULAR);
 
 async function getMovies(url) {
@@ -30,7 +30,7 @@ function showMovies(data) {
   // Очищаем предыдущие фильмы
   document.querySelector(".movies").innerHTML = "";
 
-  data.items.forEach((movie) => { /*массив с фильмами */
+  data.items.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
