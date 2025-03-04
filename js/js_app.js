0fe24d1f6b8202d3b731ab0531511b70458b3e15
KoraOrvatnew <mironovich.alena83@gmail.com>
2025-03-04 19:19:04 +0300

    modal2

--- js/js_app.js
+++ js/js_app.js
@@ -26,52 +26,56 @@ function showMovies(data) {
   const moviesEl = document.querySelector(".movies");
 
   // Очищаем предыдущие фильмы
-  document.querySelector(".movies").innerHTML = "";
-console.log(typeof(data));
-  Object.values(data).forEach((movie) => {
+  moviesEl.innerHTML = "";
+
+  Object.values(data).forEach((movie, index) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
         <div class="movie_cover-inner">
-        <img
-          src="${movie.img}" /*прьевью фильмов*/
-          class="movie_cover"
-          alt="${movie.name}" /*название*/
-        />
-        <div class="movie_cover--darkened"></div>
-      </div>
-      < class="movie_info">
-      <div>
-                <button class="btnwatchLater">Хочу посмотреть</button>
-              </div>
-        <div class="movie_title">Название: ${movie.name}</div>
-        <div class="movie_category">Жанр: ${movie.genre}</div>
-
-
-        <button id="openModal">Нажми меня!</button>
-
-        <div id="modal" class="modal">
-          <div class="modal-content">
-            <button class="close">close</button>
-            <p>Привет, я всплывающее окно!</p>
-          </div>
+          <img
+            src="${movie.img}" /*превью фильмов*/
+            class="movie_cover"
+            alt="${movie.name}" /*название*/
+          />
+          <div class="movie_cover--darkened"></div>
         </div>
-       
-    
-    </div>
-        ${movie.rating &&
+        <div class="movie_info">
+          <div>
+            <button class="btnwatchLater">Хочу посмотреть</button>
+          </div>
+          <div class="movie_title">Название: ${movie.name}</div>
+          <div class="movie_category">Жанр: ${movie.genre}</div>
+          <button class="openModal" data-index="${index}">Нажми меня!</button>
+          ${movie.rating &&
             `
-        <div class="movie_average--${getClassByRate(
-          movie.rating
-        )}
-          ">${movie.rating}</div>
-        `}
-      </div>
-        
-      
-        `;
+            <div class="movie_average--${getClassByRate(movie.rating)}">
+              ${movie.rating}
+            </div>
+          `}
+        </div>
+      `;
     moviesEl.appendChild(movieEl);
   });
+
+  // Настройка обработчиков событий для модального окна
+  const modal = document.querySelector('#modal');
+  const openModalButtons = document.querySelectorAll('.openModal');
+  const closeButton = document.querySelector('.close');
+
+  openModalButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      modal.style.display = 'block';
+    });
+  });
+
+  closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
 }
-
-
--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,17 @@
-const modal = document.querySelector('#modal');
-const btn = document.querySelector('#openModal');
-const close = document.querySelector('.close');
+// const modal = document.querySelector('#modal');
+// const btn = document.querySelector('#openModal');
+// const close = document.querySelector('.close');
 
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
+// btn.onclick = function () {
+//   modal.style.display = 'block';
+// };
 
-close.onclick = function () {
-  modal.style.display = 'none';
-};
+// close.onclick = function () {
+//   modal.style.display = 'none';
+// };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = 'none';
+//   }
+// };
