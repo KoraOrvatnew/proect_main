fc9aae266fc8eaccbd65fb41cfb5d4739cf92d33
KoraOrvatnew <mironovich.alena83@gmail.com>
2025-03-04 12:02:50 +0300

    zamenabazy

--- js_app2.js
+++ js_app2.js
@@ -1,6 +1,6 @@
-const API_KEY = "529c790f-3578-406b-94dd-d8956b7d306b";
-const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1";
-// const API_URL_POPULAR = "https://media-new-7c859-default-rtdb.firebaseio.com/"
+// const API_KEY = "529c790f-3578-406b-94dd-d8956b7d306b";
+// const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1";
+const API_URL_POPULAR = "https://media-new-7c859-default-rtdb.firebaseio.com/films.json"
 // const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 getMovies(API_URL_POPULAR);
 
@@ -8,12 +8,13 @@ async function getMovies(url) {
   const resp = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
-      "X-API-KEY": API_KEY,
+      // "X-API-KEY": API_KEY,
     },
   });
   const respData = await resp.json();
   showMovies(respData);
 }
+
 
 function getClassByRate(vote) { /*рейтинг */
   if (vote >= 7) {
@@ -30,16 +31,16 @@ function showMovies(data) {
 
   // Очищаем предыдущие фильмы
   document.querySelector(".movies").innerHTML = "";
-
-  data.items.forEach((movie) => {
+console.log(typeof(data));
+  Object.values(data).forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
         <div class="movie_cover-inner">
         <img
-          src="${movie.posterUrlPreview}" /*прьевью фильмов*/
+          src="${movie.img}" /*прьевью фильмов*/
           class="movie_cover"
-          alt="${movie.nameRu}" /*название*/
+          alt="${movie.name}" /*название*/
         />
         <div class="movie_cover--darkened"></div>
       </div>
@@ -47,16 +48,14 @@ function showMovies(data) {
       <div>
                 <button class="btnwatchLater">Хочу посмотреть</button>
               </div>
-        <div class="movie_title">Название: ${movie.nameRu}</div>
-        <div class="movie_category">Жанр: ${movie.genres.map( /*жанр*/
-          (genre) => ` ${genre.genre}`
-        )}</div>
-        ${movie.ratingKinopoisk &&
+        <div class="movie_title">Название: ${movie.name}</div>
+        <div class="movie_category">Жанр: ${movie.genre}</div>
+        ${movie.rating &&
             `
         <div class="movie_average--${getClassByRate(
-          movie.ratingKinopoisk
+          movie.rating
         )}
-          ">${movie.ratingKinopoisk}</div>
+          ">${movie.rating}</div>
         `}
       </div>
         
@@ -65,8 +64,5 @@ function showMovies(data) {
     moviesEl.appendChild(movieEl);
   });
 }
-
-
-
 
 
