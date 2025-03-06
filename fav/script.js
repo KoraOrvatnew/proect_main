9a392065d75947f1ca9050ffe847d40eb58a0060
KoraOrvatnew <mironovich.alena83@gmail.com>
2025-03-06 21:22:44 +0300

    itog

--- fav/script.js
+++ fav/script.js
@@ -1,14 +1,32 @@
-// script.js
-
-// Функция для добавления товара в избранное
-function addToFavorites(product) {
+// Функция для удаления фильма из избранного
+function removeFromFavorites(movieName) {
     let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    
-    if (!favorites.includes(product)) {
-        favorites.push(product);
+    if (favorites.includes(movieName)) {
+        favorites = favorites.filter(favorite => favorite !== movieName);
         localStorage.setItem('favorites', JSON.stringify(favorites));
-        alert(`${product} добавлен в избранное!`);
+        alert(`${movieName} - фильм удален из избранного!`);
+        // Обновление списка избранных фильмов после удаления
+        displayFavorites();
     } else {
-        alert(`${product} уже в избранном!`);
+        alert(`${movieName} - фильм не найден в избранном!`);
     }
+    displayFavorites();
 }
+function displayFavorites(){
+// Получаем избранные товары из localStorage и отображаем их
+const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+const favoritesList = document.getElementById('favoritesList');
+const deleteContainerBtn = document.getElementById('delete-container')
+if (favorites.length === 0) {
+    favoritesList.innerHTML = '<li>Нет избранных фильмов</li>';
+} else {
+  favoritesList.innerHTML = "";
+     favorites.forEach(product => {
+        const li = document.createElement('li');
+         li.innerHTML = product + ' <button class="BtnDel" onclick="removeFromFavorites(\'' + product.toString() + '\')">Удалить</button>';
+       
+        favoritesList.appendChild(li);
+     });
+}
+}
+displayFavorites();
--- main/js_app.js
+++ main/js_app.js
@@ -129,3 +129,51 @@ function displayFavorites() {
 
 // Вызываем функцию для отображения избранных фильмов при загрузке
 displayFavorites();
+
+    // Функция для удаления фильма из избранного
+      function removeFromFavorites(movieName) {
+          let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+          if (favorites.includes(movieName)) {
+              favorites = favorites.filter(favorite => favorite !== movieName);
+              localStorage.setItem('favorites', JSON.stringify(favorites));
+              alert(`${movieName} - фильм удален из избранного!`);
+              // Обновление списка избранных фильмов после удаления
+              displayFavorites();
+          } else {
+              alert(`${movieName} - фильм не найден в избранном!`);
+          }
+          displayFavorites();
+      }
+      function displayFavorites(){
+      // Получаем избранные товары из localStorage и отображаем их
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+      const favoritesList = document.getElementById('favoritesList');
+      const deleteContainerBtn = document.getElementById('delete-container')
+      if (favorites.length === 0) {
+          favoritesList.innerHTML = '<li>Нет избранных фильмов</li>';
+      } else {
+        favoritesList.innerHTML = "";
+           favorites.forEach(product => {
+              const li = document.createElement('li');
+               li.innerHTML = product + ' <button class="BtnDel" onclick="removeFromFavorites(\'' + product.toString() + '\')">Удалить</button>';
+             
+              favoritesList.appendChild(li);
+           });
+      }
+    }
+    displayFavorites();
+      
+  /*поиск */
+const form = document.querySelector("form");
+const search = document.querySelector(".header_search");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+  if (search.value) {
+    getMovies(apiSearchUrl);
+
+    search.value = "";/*очистка введенного в поиск текста*/
+  }
+});
